9104af07d5138fb435c2c9070e610bd8e9abec57
MangirdasRubavicius <127213006+MangirdasRubavicius@users.noreply.github.com>
2023-04-06 20:22:30 +0300

    Update Multiplication_for_kids.py

--- Multiplication_for_kids.py
+++ Multiplication_for_kids.py
@@ -1,24 +1,24 @@
 from random import randint
-t=0
-n=0
+
+t = 0
+n = 0
 if __name__ == "__main__":
     for i in range(10):
-        number_1 = randint(1,10)
-        number_2 = randint(1,10)
+        number_1 = randint(1, 10)
+        number_2 = randint(1, 10)
         number_3 = number_1 * number_2
-        number_3 = float(number_3)
         print(number_1, "*", number_2, "=")
-        answer=input("What is the answer? : " )
-        answer = float(answer)
+        answer = int(input("What is the answer? : "))
+
         if answer == number_3:
-            t=t+1
-            number=number+1
+            t = t + 1
+            print("Correct!")
         else:
-            n=n+1
+            n = n + 1
+            print("Wrong. The correct answer is :", number_3)
+
 if t >= 7:
     passing = "passed"
-    
 else:
     passing = "failed"
-    print("You answered: ", t ," correctly, and", n, " incorrectly, you ", passing , " the test.")
-
+print("You answered: ", t, " correctly, and", n, " incorrectly, you ", passing, " the test.")
